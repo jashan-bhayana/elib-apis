d1cb5c4f32fb39f0178b58077feff2da42f5f6ef
jashan-bhayana <jashanbhayana5@gmail.com>
2024-05-09 00:23:08 +0530

    change imports

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import createHttpError, { HttpError } from 'http-errors'
 import { config } from './config/config'
 import globalErrorHandler from './middlewares/globalErrorHandler'
-import userRouter from './user/userRoutes'
+import userRouter from './user/userRouter'
 
 
 const app = express()
