1d50554c8001d4dc217e64ae5031b0c162d2e823
jashan-bhayana <jashanbhayana5@gmail.com>
2024-05-14 22:46:20 +0530

    add jwt middleware configuration

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,6 +2,7 @@ const express = require("express");
 import path from "node:path";
 import multer from "multer";
 import { createBook } from "./bookController";
+import authenticate from "../middlewares/authenticate";
 
 const upload = multer({
   dest: path.resolve(__dirname, "../../public/data/uploads"), // .. means 1 folder back,multer will create folder by itself
@@ -12,6 +13,7 @@ const bookRouter = express.Router();
 
 bookRouter.post(
   "/",
+  authenticate ,
   upload.fields([
     { name: "coverImage", maxCount: 1 },
     { name: "file", maxCount: 1 },
